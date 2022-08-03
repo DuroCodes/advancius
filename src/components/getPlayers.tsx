969940d8ec67a32206b87c721ceb5dd35198875c
DuroCodes <durometergfx@gmail.com>
2022-08-03 19:32:15 -0400

    feat: add type to players in getPlayers.tsx

--- src/components/getPlayers.tsx
+++ src/components/getPlayers.tsx
@@ -1,11 +1,25 @@
 import React, { useState, useEffect } from 'react';
 
+
+export interface IPlayers {
+  online: number;
+  max: number;
+}
+
+export interface IServerData {
+  ip: string;
+  port: number;
+  players: IPlayers;
+  version: string;
+  online: boolean;
+}
+
 export default function getPlayers() {
   const [playerInfo, setPlayerInfo] = useState('');
 
   const fetchData = async () => {
     const response = await fetch('https://api.mcsrvstat.us/2/mc.advancius.net');
-    const { players } = await response.json();
+    const { players } = await response.json() as IServerData;
     setPlayerInfo(`Players: ${players?.online || 0}/${players?.max || 0}`);
   };
 
